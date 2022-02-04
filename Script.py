657f8e4570fe70cc31516b079b8e3e8bdb2c79cf
kinh122 <97784528+kinh122@users.noreply.github.com>
2022-02-04 08:42:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,21 +1,18 @@
 class script(object):
     START_TXT = """HELLO {},
-I'M A BOT FOR <a href='https://t.me/joinchat/TuzhGj7SC871arDV'>M4 MALLUMOVIES</a>, YOU CAN'T ADD ME TO YOUR GROUP🤭"""
+I'M A BOT FOR <a href='https://t.me/joinchat/TuzhGj7SC871arDV'>LEGUND'S MOVIES</a>, YOU CAN'T ADD ME TO YOUR GROUP🤭"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: ADVANCED AUTO FILTER BOT
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: JOEL KURIAN BIJU
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: HAREESH M
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""  
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a HAREESH M</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and I will respond whenever a keyword is found the message
